dd75ccdf985555911b31e1d7eced0951d0029fc3
inti0331 <inti0331@DESKTOP-HLSFT8K>
2018-03-29 14:57:06 +0200

    Valentin: ajout de model: Categorie, Client, Produit

--- Projet_siteEcommerce/src/fr/adaming/model/Produit.java
+++ Projet_siteEcommerce/src/fr/adaming/model/Produit.java
@@ -1,5 +1,107 @@
 package fr.adaming.model;
 
-public class Produit {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="produits")
+public class Produit implements Serializable {
+	
+	// Attributs
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_produit")
+	private Long idProduit;
+	private String designation;
+	private String description;
+	private double prix;
+	private int quantite;
+	private boolean selectionne;
+	private byte[] photo;
+	
+	// Transformation association UML en Java
+	// ManyToOne avec Categorie
+	@ManyToOne
+	@JoinColumn(name="categorie_id", referencedColumnName="id_categorie")
+	private Categorie categorie;
+	// OneToMany avec LigneCommande
+	
+	// Constructeurs (vide, sans id et avec id)
+	public Produit() {
+		super();
+	}
+	public Produit(String designation, String description, double prix, int quantite, boolean selectionne,
+			byte[] photo) {
+		super();
+		this.designation = designation;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photo = photo;
+	}
+	public Produit(Long idProduit, String designation, String description, double prix, int quantite,
+			boolean selectionne, byte[] photo) {
+		super();
+		this.idProduit = idProduit;
+		this.designation = designation;
+		this.description = description;
+		this.prix = prix;
+		this.quantite = quantite;
+		this.selectionne = selectionne;
+		this.photo = photo;
+	}
+	
+	// Getters et setters
+	public Long getIdProduit() {
+		return idProduit;
+	}
+	public void setIdProduit(Long idProduit) {
+		this.idProduit = idProduit;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	public int getQuantite() {
+		return quantite;
+	}
+	public void setQuantite(int quantite) {
+		this.quantite = quantite;
+	}
+	public boolean isSelectionne() {
+		return selectionne;
+	}
+	public void setSelectionne(boolean selectionne) {
+		this.selectionne = selectionne;
+	}
+	public byte[] getPhoto() {
+		return photo;
+	}
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+	
 }
