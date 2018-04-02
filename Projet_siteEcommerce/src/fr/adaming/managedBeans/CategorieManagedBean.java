0ba213dbcc4e8532fc57d5491e1fb5f9327d0e3b
inti0331 <inti0331@DESKTOP-HLSFT8K>
2018-04-02 21:06:27 +0200

    Val: commit test

--- Projet_siteEcommerce/src/fr/adaming/managedBeans/CategorieManagedBean.java
+++ Projet_siteEcommerce/src/fr/adaming/managedBeans/CategorieManagedBean.java
@@ -11,6 +11,8 @@ import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 
+import org.primefaces.event.RowEditEvent;
+
 import fr.adaming.model.Categorie;
 import fr.adaming.service.ICategorieService;
 
@@ -21,7 +23,7 @@ public class CategorieManagedBean {
 	// Attributs
 	private Categorie categorie;
 	private List<Categorie> listeCategories;
-	private boolean rendered;	// pour le rendered de la vue
+	private boolean rendered; // pour le rendered de la vue
 	private HttpSession session;
 
 	// Transformation de l'association UML en Java
@@ -31,14 +33,14 @@ public class CategorieManagedBean {
 	// Constructeur vide
 	public CategorieManagedBean() {
 		this.categorie = new Categorie();
-		this.rendered=false;
+		this.rendered = false;
 	}
 
 	@PostConstruct
 	public void init() {
 		this.session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
 	}
-	
+
 	// Getters et setters
 	public Categorie getCategorie() {
 		return categorie;
@@ -55,13 +57,15 @@ public class CategorieManagedBean {
 	public void setListeCategories(List<Categorie> listeCategories) {
 		this.listeCategories = listeCategories;
 	}
+
 	public boolean isRendered() {
 		return rendered;
 	}
+
 	public void setRendered(boolean rendered) {
 		this.rendered = rendered;
 	}
-	
+
 	public HttpSession getSession() {
 		return session;
 	}
@@ -104,26 +108,47 @@ public class CategorieManagedBean {
 		}
 	}
 
+	public String editLink() {
+		this.categorie = categorieService.get(categorie);
+		return "#";
+	}
+
+	public void onRowEdit(RowEditEvent event) {
+		categorieService.update((Categorie) event.getObject());
+		listeCategories = categorieService.getAll();
+		session.setAttribute("listeCategories", listeCategories);
+		FacesMessage msg = new FacesMessage("Catégorie modifiée", ((Categorie) event.getObject()).getNomCategorie());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public void onRowCancel(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Modification annulée", ((Categorie) event.getObject()).getNomCategorie());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
 	public String supprimer() {
 		int verif = categorieService.delete(categorie);
 		if (verif != 0) {
 			// Récupérer la liste des catégories mise à jour
 			listeCategories = categorieService.getAll();
-			return "testVal";
+			// L'ajouter à la session
+			session.setAttribute("listeCategories", listeCategories);
+			return "#";
 		} else {
 			// Si erreur
 			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("La catégorie indiquée n'existe pas."));
-			return "testVal";
+			return "#";
 		}
 	}
-	
-	public String consulter(){
-		try{
-			this.categorie=categorieService.get(categorie);
-			this.rendered=true;
-		} catch(EJBTransactionRolledbackException e) {
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("La catégorie recherchée n'existe pas."));
-			this.rendered=false;
+
+	public String consulter() {
+		try {
+			this.categorie = categorieService.get(categorie);
+			this.rendered = true;
+		} catch (EJBTransactionRolledbackException e) {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage("La catégorie recherchée n'existe pas."));
+			this.rendered = false;
 		}
 		return "testVal";
 	}
